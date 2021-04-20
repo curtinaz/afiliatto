ee7065a61e3565fa53c24d3b96f2da5184f9a51e
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-20 17:46:43 -0300

    Update afiliatto.js

--- afiliatto.js
+++ afiliatto.js
@@ -2,6 +2,6 @@
 
 function isOk() {
     console.log("teste");
-    var inputHolder = document.querySelector('#afiliatte_id').value;
-    window.alert(inputHolder);
+    // var inputHolder = document.querySelector('#afiliatte_id').value;
+    // window.alert(inputHolder);
 }
