55d3e2710321182e624018b220b8298050d76cc9
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-20 17:47:18 -0300

    Update afiliatto.js

--- afiliatto.js
+++ afiliatto.js
@@ -1,6 +1,6 @@
 // adiciona o valor do input à uma variável
 
-function isOk() {
+var isOk = () => {
     console.log("teste");
     // var inputHolder = document.querySelector('#afiliatte_id').value;
     // window.alert(inputHolder);
