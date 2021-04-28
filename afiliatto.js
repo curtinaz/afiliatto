cdf651056374704ec7ea9baa297ecd56986d64ec
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-28 13:37:45 -0300

    Update afiliatto.js

--- afiliatto.js
+++ afiliatto.js
@@ -1,3 +1,3 @@
-let search = location.search.slice(1);
+let search = location.search.substring(1);
 
 JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
