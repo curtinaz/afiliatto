0d7f962f1aa9247b87d571238ff15635bdd9834a
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-29 13:39:58 -0300

    Update afiliatto.js

--- afiliatto.js
+++ afiliatto.js
@@ -1,3 +1,4 @@
 let search = location.search.substring(1);
 
 search = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
+
