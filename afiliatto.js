f1cf1cf4729118ea96d1456f1e84c9365c752772
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-05-05 13:50:19 -0300

    Update afiliatto.js

--- afiliatto.js
+++ afiliatto.js
@@ -6,11 +6,12 @@ if (param.sujeito == "") {
     let hasparam = false;
 } else {
     var sujeito = param.sujeito;
-    var sujeito_qtd = document.querySelectorAll(".sujeito");
+    var sujeito_qtd = document.querySelectorAll(".sujeito").length;
     var changing = 0;
     
     for (changing = 0; changing <= sujeito_qtd; changing++) {
 
+        
         console.log("temporario");
 
     }
