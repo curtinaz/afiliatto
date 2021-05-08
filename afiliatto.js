5449d10e0a470338590639cece3312906b7699a3
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-05-08 16:47:24 -0300

    Nome do Lead

--- afiliatto.js
+++ afiliatto.js
@@ -7,7 +7,7 @@ var param = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + 
 if (param.ndl == "") {
     let hasparam = false; // O código verifica se existe o parâmetro "ndl"
 } else {
-    var ndl = param.ndl; // O código cria a variável ndl, que vai mudar o valor das classes ndl.
+    var ndl = param.ndl; // O código cria a variável ndl (Nome do Lead), que vai mudar o valor das classes ndl.
     var ndl_qtd = document.querySelectorAll(".ndl").length; // O código question a quantidade de classes ".ndl" que existem no site.
     var changing = 0; // O código define um valor "i" para o laço 'for'.
     
