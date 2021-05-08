9760ba015db13879e325d4eab574e79447fa5223
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-05-08 16:59:42 -0300

    define changing 0 ao final do codigo

--- afiliatto.js
+++ afiliatto.js
@@ -5,12 +5,21 @@ let search = location.search.substring(1);
 var param = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) }) 
 
 if (param.ndl == "") {
-    let hasparam = false; // O código verifica se existe o parâmetro "ndl"
+    let hasparam = false;
 } else {
     var ndl = param.ndl; // O código cria a variável ndl (Nome do Lead), que vai mudar o valor das classes ndl.
     var ndl_qtd = document.querySelectorAll(".ndl").length; // O código question a quantidade de classes ".ndl" que existem no site.
     var changing = 0; // O código define um valor "i" para o laço 'for'.
     
+    for (changing = 0; changing <= ndl_qtd; changing++) {
+
+        document.querySelectorAll(".ndl")[changing].innerHTML = ndl;
+        console.log("Nome do lead foi alterado");
+
+    }
+
+    var changing = 0;
+
     for (changing = 0; changing <= ndl_qtd; changing++) {
 
         document.querySelectorAll(".ndl")[changing].innerHTML = ndl;
