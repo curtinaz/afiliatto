e9053d10a32f39d1f0cb4eb82c42da81cfd8cd47
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-05-08 16:46:44 -0300

    Update afiliatto.js

--- afiliatto.js
+++ afiliatto.js
@@ -4,17 +4,17 @@ let search = location.search.substring(1);
 // O código transforma os parâmetros em um JSON. (levando em conta os caracteres especiais)
 var param = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) }) 
 
-if (param.sujeito == "") {
-    let hasparam = false; // O código verifica se existe o parâmetro "sujeito"
+if (param.ndl == "") {
+    let hasparam = false; // O código verifica se existe o parâmetro "ndl"
 } else {
-    var sujeito = param.sujeito; // O código cria a variável sujeito, que vai mudar o valor das classes sujeito.
-    var sujeito_qtd = document.querySelectorAll(".sujeito").length; // O código question a quantidade de classes ".sujeito" que existem no site.
+    var ndl = param.ndl; // O código cria a variável ndl, que vai mudar o valor das classes ndl.
+    var ndl_qtd = document.querySelectorAll(".ndl").length; // O código question a quantidade de classes ".ndl" que existem no site.
     var changing = 0; // O código define um valor "i" para o laço 'for'.
     
-    for (changing = 0; changing <= sujeito_qtd; changing++) {
+    for (changing = 0; changing <= ndl_qtd; changing++) {
 
-        document.querySelectorAll(".sujeito")[changing].innerHTML = sujeito;
-        console.log("Nome do Sujeito foi alterado");
+        document.querySelectorAll(".ndl")[changing].innerHTML = ndl;
+        console.log("Nome do lead foi alterado");
 
     }
 }
